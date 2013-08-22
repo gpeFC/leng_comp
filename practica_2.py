c3e0887651e863617162459c7b6938fc98a713c0
emanuel <emanuel.garp@gmail.com>
2013-08-22 18:24:39 -0500

    captura de un error en la funcion con un try-except

--- practica_2.py
+++ practica_2.py
@@ -15,8 +15,11 @@ def some_function(onevalue, other_one, with_default="ok", next = "still with val
     print with_default
 
 if __name__ == "__main__":
-    some_function("hi", "there")
-    print 
-    some_function("hi", "again", "kid")
-    print 
-    some_function("hi") # tricky one.
+    try:
+        some_function("hi", "there")
+        print 
+        some_function("hi", "again", "kid")
+        print 
+        some_function("hi") # tricky one.
+    except:
+        print "Error en la función."
