4662e21170cf31fde97c8f7a20a767d8f4cd6d3a
emanuel <emanuel.garp@gmail.com>
2013-09-05 18:09:34 -0500

    programa final

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -10,7 +10,7 @@
 class BinarySearch:
 	def __init__(self, num_lista):
 		self.vector_num = num_lista
-		self.dim_vector = len(self.vector_num)
+		self.valor_bool = False
 
 	def ordena_vector(self, num_lista):
 		for num in range(len(num_lista)):
@@ -19,13 +19,37 @@ class BinarySearch:
 					num_lista[num], num_lista[item] = num_lista[item], num_lista[num]
 
 	def buscar_numero(self, numero, num_lista):
-		index_temp = (self.dim_vector)/2
-		num_temp = num_lista[index_temp]
-		if numero == num_temp:
+		index_temp = (len(num_lista))/2
+		if index_temp > 0:
+			num_temp = num_lista[index_temp]
+			if numero == num_temp:
+				self.valor_bool = True
+			else:
+				if numero > num_temp:
+					self.buscar_numero(numero, num_lista[index_temp:])
+				else:
+					self.buscar_numero(numero, num_lista[:index_temp])
+		elif index_temp == 0:
+			num_temp = num_lista[index_temp]
+			if numero == num_temp:
+				self.valor_bool = True
+	def resultado_busqueda(self, numero, num_lista):
+		self.buscar_numero(numero, num_lista)
+		if self.valor_bool:
 			return True
 		else:
-			izq_lista = num_lista[:index_temp]
-			der_lista = num_lista[index_temp:]
-			buscar_numero(numero, izq_lista)
-			buscar_numero(numero, der_lista)
-		return False
+			return False
+
+if __name__ == "__main__":
+	#mi_lista_0 = [3, -5, 2, -6, 8, -9, 1, 0, -4, 7]
+	mi_lista_0 = [3, 5, 2, 6, 8, 9, 1, 0, 4, 7]
+	print "Lista Original: ", mi_lista_0
+	nueva_busqueda = BinarySearch(mi_lista_0)
+	nueva_busqueda.ordena_vector(mi_lista_0)
+	print "Lista Ordenada: ", mi_lista_0
+	num_busq = int(raw_input("Numero a buscar: "))
+	valor = nueva_busqueda.resultado_busqueda(num_busq, mi_lista_0)
+	if valor:
+		print "Número encontrado."
+	else:
+		print "Número no encontrado."
