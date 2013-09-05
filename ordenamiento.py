a1e65e8b937943eec0fc41a28d41eb0b3058790e
emanuel <emanuel.garp@gmail.com>
2013-09-05 07:29:22 -0500

    cambios varios

--- ordenamiento.py
+++ ordenamiento.py
@@ -8,42 +8,33 @@
 ========================================================================"""
 
 
-
-
+mi_lista_0 = [3, -5, 2, -6, 8, -9, 1, 0, -4, 7]
 mi_lista_1 = [3, 5, 2, 6, 8, 9, 1, 0, 4, 7]
-mi_lista_2 = []
+
+def selection_sort(num_lista):
+	count = 0
+	for num in range(len(num_lista)):
+		num_min = num_lista[count]
+		for item in num_lista:
+			if item <= num_min:
+				num_min = num
+		num_lista[num], num_min = num_min, num_lista[num]
+		count += 1
+		print num_lista
+	return num_lista
+
+def insertion_sort(num_lista):
+	for i in range(len(num_lista)):
+		index = -1
+		for j in range(i, len(num_lista)):
+			if num_lista[index] < num_lista[i]:
+				num_lista[i], num_lista[index] = num_lista[index], num_lista[i]
+			index -= 1
+		print num_lista
 
 print "Lista a Ordenar: ", mi_lista_1
-count = 0
-for num in range(len(mi_lista_1)):
-	num_min = mi_lista_1[count]
-	for item in mi_lista_1:
-		if item <= num_min:
-			num_min = num
-	mi_lista_2.append(num_min)
-	count += 1
-	print mi_lista_2
+mi_lista_2 = selection_sort(mi_lista_1)
 print "Lista Ordenada: ", mi_lista_2
-
 print "-"*75
-
-print "Lista a Ordenar: ", mi_lista_1
-i = 0
-j = 1
-num_flag = True
-while num_flag:
-	if mi_lista_1[j] < mi_lista_1[i]:
-		if i > 0:
-			m = 0
-			m += i
-			for n in range(0, m):
-				if mi_lista_1[j] < mi_lista_1[m]:
-					mi_lista_1[m], mi_lista_1[j] = mi_lista_1[j], mi_lista_1[m]
-				m -= 1
-	i += 1
-	j += 1
-	if j == len(mi_lista_1):
-		i = 0
-		j = 1
-	print mi_lista_1
-	var = raw_input("entrada: ")
+print "Lista a Ordenar: ", mi_lista_0
+insertion_sort(mi_lista_0)
