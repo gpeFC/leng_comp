17a815d4372c6c848f0107425e0f749978fce14a
emanuel <emanuel_gp@hotmail.com>
2013-10-31 00:26:24 -0600

    Creacion del Archivo Fuente para el script del Parser de la Calculadora.

---
+++ parser_calc/parser_calc.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+
+"""========================================================================
+    Octubre / 2013
+    Emanuel G.P.
+    Lenguajes y Compiladores.
+    Facultad de Ciencias, UAEM.
+                        >>Parser-Calculadora<<
+========================================================================"""
+
+
+
+class Parser:
+    pass 
+
+
+
+if __name__ == "__main__":
+    print "Parser para una Calculadora...\n"
+    
+
+
